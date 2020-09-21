3ddd47a95236da944deecf9c123a8e5cc2668b6d
PJandl <pjandl@gmail.com>
2020-09-21 13:27:07 -0300

    Update main22.c
    A entrada fornecida pelo usuário não foi validada.
    A verificação de número primo só pode ocorrer quando número >= 2.

--- cap03/main22.c
+++ cap03/main22.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main() {
-	int numero;		// declaração do valor a ser testado
+	int numero;		// declaraÃ§Ã£o do valor a ser testado
 	int i, primo = 1;	// variavel auxiliar
 
 	printf("Teste para numeros primos\n");
 	printf("Digite um inteiro para verificar se primo: ");
 	scanf("%d", &numero);
 
-    // Verifica entrada
-    if (numero < 2) {
-        printf("Inteiro < 2 nao pode ser verificado\n");
-        return 0;
-    }
-    // laço para todas as possibilidade de teste
+    	// Verifica entrada
+    	if (numero < 2) {
+        	printf("Inteiro < 2 nao pode ser verificado\n");
+        	return 0;
+    	}
+        // laÃ§o para todas as possibilidade de teste
 	for (i=2; i<=numero/2; i++) {
 		// verifica se numero divisivel por i
 		if (numero%i==0) {
-			primo = 0; 	// caso afirmativo numero nao é primo
-			break;		// nao é preciso prosseguir com laço
+			primo = 0; 	// caso afirmativo numero nao Ã© primo
+			break;		// nao Ã© preciso prosseguir com laÃ§o
 		}
 	}
 	// uso do operador ?: para formatar mensagem final
